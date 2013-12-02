9b9d93f1947cb7d9a688f7352a69f7f0e9c26b8c
Maven <v@maweis.com>
2013-12-02 19:38:36 +0800

    add import wxaccount urls module

--- wxplatform/wxplatform/urls.py
+++ wxplatform/wxplatform/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-import wxaccount
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wx.views.home', name='home'),
-    url(r'^wx/', include(wxaccount.urls)),
+    url(r'^wx/', 'wxaccount.views.urls'),
     url(r'^admin/', include(admin.site.urls)),
 )
